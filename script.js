e09f143fa099aad07d16ebd0a7cc610a2edb1773
BAGGS271 <124683933+BAGGS271@users.noreply.github.com>
2025-04-14 15:35:04 +0100

    Added new functions and button
    Created functions to get a number form a user input on a button press. The square route is then taken from this to create the grid.

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 //Declaring conmtainer Div
 const container = document.getElementById("container");
 
-//function to create 16 divs in a loop
+//function to get a number from the user
+function getNumber() {
+    let number = prompt("enter a number between 1 - 100");
+    if (number >= 100 || number <= 1) {
+        alert("Please enter a valid number");
+        return getNumber();
+    }
+    return number;
+}
+
+//function to create divs changing on number input and getting the square route
 function createDiv() {
-    for (let i = 0; i < 256; i++) {
+    let number = getNumber();
+    number = number * number;
+
+    for (let i = 0; i < number; i++) {
         const div = document.createElement("div");
+        div.classList.add("sketchBox");
         container.appendChild(div);
     }
 }
 
-createDiv();
+//gets number and createws div on button press
+const sizeButton = document.getElementById("sizeButton")
+sizeButton.addEventListener("click", createDiv);
+
+
+//changes div colour when hovered over
+    const sketchBoxes = document.querySelectorAll(".sketchBox");
+    sketchBoxes.forEach((sketchBox) => {
+        sketchBox.addEventListener("mouseover", () => {
+            sketchBox.style.backgroundColor = "blue";
+        });
+    });
